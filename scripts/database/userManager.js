b8269bcb33f76e9ed5d1c817aae75acfff3dd716
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-04-21 10:30:33 -0300

    firebase auth and db connected. At login page, create account and login working. Next steps manage Routes and config the data structure for users

---
+++ scripts/database/userManager.js
@@ -0,0 +1,49 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC6VYT4VLAsguid-o5qIv7ZQVMRu-BdLNM",
+  authDomain: "jackedplanner.firebaseapp.com",
+  projectId: "jackedplanner",
+  storageBucket: "jackedplanner.appspot.com",
+  messagingSenderId: "568897936410",
+  appId: "1:568897936410:web:22a686867e103a1137c5a3",
+  measurementId: "G-F7GEL0SCTG"
+};
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
+const auth = firebase.auth()
+
+function login(x, y) {
+  auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(() => {
+    auth.signInWithEmailAndPassword(x, y).then(() => {
+      console.log(`user logged in: ${auth.currentUser}`)
+    }).catch(err => console.log(err))
+  }).catch(err => console.log(err))
+}
+
+function createNewAccount(x, y, z) {
+  auth.createUserWithEmailAndPassword(x, y).then(cred => {
+    return db.collection('users').doc(cred.user.uid).set({
+      userEmail: x,
+      userName: z
+    })
+  }).then(() => {
+    console.log('new user created')
+  }).catch(err => {
+    console.log(err)
+  })
+}
+
+function logout() {
+  auth.signOut().then(() => {
+    console.log('User logged Out!')
+  }).catch(err => {
+    console.error(err)
+  })
+}
+
+auth.onAuthStateChanged(user => {
+  if (user) {
+    console.log(user.currentUser)
+  } else { console.log('ninguém loggado!') }
+})
+
+export { createNewAccount, login, logout }
--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,57 @@
-const close = document.getElementsByClassName('close')
+import { createNewAccount, login } from "./database/userManager.js";
+
+const loginForm = document.getElementById('loginForm')
+const userInput = document.getElementById('user')
+const passwordInput = document.getElementById('password')
+var user = ''
+userInput.oninput = () => user = userInput.value
+var password = ''
+passwordInput.oninput = () => password = passwordInput.value
+
+loginForm.onsubmit = (event) => {
+  event.preventDefault()
+  login(user, password)
+}
 
 const newUserBox = document.getElementById('create-box')
 const createForm = document.getElementById('create-form')
 const create = document.getElementById(`create`)
-const createBtn = document.getElementById('create-submit')
 const createMsg = document.getElementById('create-msg')
 
-const forgotBox = document.getElementById('forgot-box')
-const forgotForm = document.getElementById('forgot-form')
-const forgot = document.getElementById(`forgot`)
-const forgotBtn = document.getElementById('forgot-btn')
-const forgotMsg = document.getElementById('forgot-msg')
-
-
 create.onclick = () => newUserBox.classList.toggle('hide')
-forgot.onclick = () => forgotBox.classList.toggle('hide')
-
-close[0].onclick = () => forgotBox.classList.toggle('hide')
-close[1].onclick = () => newUserBox.classList.toggle('hide')
-
+const newUserNameInput = document.getElementById('userName')
+var userName = ''
+newUserNameInput.oninput = () => {
+  userName = newUserNameInput.value
+}
+const newUserEmailInput = document.getElementById('userEmail')
+var userEmail = ''
+newUserEmailInput.oninput = () => {
+  userEmail = newUserEmailInput.value
+}
+const newUserPasswordInput = document.getElementById('userPassword')
+var userPassword = ''
+newUserPasswordInput.oninput = () => {
+  userPassword = newUserPasswordInput.value
+}
 createForm.onsubmit = (event) => {
   event.preventDefault()
   createMsg.classList.toggle('hide')
   setTimeout(() => {
     createMsg.classList.toggle('hide')
     newUserBox.classList.toggle('hide')
-    /* createForm.submit() */
+    createNewAccount(userEmail, userPassword, userName)  /* using firebase auth */
   }, 1500);
 }
+
+const forgotBox = document.getElementById('forgot-box')
+const forgotForm = document.getElementById('forgot-form')
+const forgot = document.getElementById(`forgot`)
+const forgotMsg = document.getElementById('forgot-msg')
+
+forgot.onclick = () => forgotBox.classList.toggle('hide')
+
+/* read the forgot inputs */
 
 forgotForm.onsubmit = (event) => {
   event.preventDefault()
@@ -35,6 +59,10 @@ forgotForm.onsubmit = (event) => {
   setTimeout(() => {
     forgotMsg.classList.toggle('hide')
     forgotBox.classList.toggle('hide')
-    /* createForm.submit() */
+    console.log('email enviado')
   }, 1500);
 }
+
+const close = document.getElementsByClassName('close')
+close[0].onclick = () => forgotBox.classList.toggle('hide')
+close[1].onclick = () => newUserBox.classList.toggle('hide')
