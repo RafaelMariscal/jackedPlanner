58fc35f9c61fbacbf7cdc984577a7689ff98d7f0
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-04-27 12:06:04 -0300

    responsive desing for mobile updated

--- public/scripts/menuHandle.js
+++ public/scripts/menuHandle.js
@@ -42,3 +42,33 @@ function position(id) {
 const logoutIcon = document.getElementById('exit')
 
 logoutIcon.onclick = logout
+const menuToggle = document.getElementById('toggle-menu')
+const aside = document.getElementById('menu-area')
+const dashArea = document.getElementById('dashboard-content')
+
+menuToggle.addEventListener('click', () => {
+  aside.classList.toggle('show')
+
+  console.log(aside.classList)
+  if (aside.classList == '') {
+    const menuIcon = `     
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M3 12H21" stroke="#084236" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M3 6H21" stroke="#084236" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M3 18H21" stroke="#084236" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>`
+    menuToggle.innerHTML = menuIcon
+    dashArea.style.padding = '1em 0 1em 1em'
+
+  } else {
+    const close = `     
+        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M17.4963 16.0872L1.93994 0.530884L0.525729 1.9451L16.0821 17.5014L17.4963 16.0872Z" fill="#8257E5"/>
+          <path d="M16.0822 0.530809L0.525879 16.0872L1.94009 17.5014L17.4964 1.94502L16.0822 0.530809Z" fill="#8257E5"/>
+        </svg>`
+    menuToggle.innerHTML = close
+    dashArea.style.padding = '1em 0 1em 12em'
+
+  }
+
+})
