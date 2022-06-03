4c0923d465bafae61cb6a05277c36c50fa9936ff
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-06-03 11:56:49 -0300

    FIX
    Sets Weight edit form CRUD was not working properly.

--- public/scripts/exercisePlan.js
+++ public/scripts/exercisePlan.js
@@ -22,7 +22,8 @@ function generateExercisePlanSets(exercise, planner, daySplitDoc) {
   setsDoneState(daySplitDoc, exercise)
 
   editSetsBtn.onclick = () => {
-    toggleEditSetsForm()
+    let unchangedSetsWeight = [...exercise.setsWeight]
+    toggleEditSetsForm(exercise, unchangedSetsWeight)
     populateExercisePlanEditForm(exercise)
     populateEditFormInputs(exercise)
     let removeSetWeightBtns = Array.from(document.getElementsByClassName('removeSetWeight'))
@@ -33,7 +34,15 @@ function generateExercisePlanSets(exercise, planner, daySplitDoc) {
     }))
   }
 
-  addNewSetBtn.onclick = addNewSetBtnFeature
+  addNewSetBtn.onclick = () => {
+    addNewSetBtnFeature(exercise)
+    let removeSetWeightBtns = Array.from(document.getElementsByClassName('removeSetWeight'))
+    removeSetWeightBtns.forEach(((removeBtn, index) => {
+      removeBtn.onclick = () => {
+        removeSetWeightFeature(exercise, index)
+      }
+    }))
+  }
   editSetsConfirmBtn.onclick = () => {
     if (confirm('Confirm Sets Weight updates?')) {
       getEditSetsWeightInputsValues(exercise, planner, daySplitDoc)
@@ -104,17 +113,18 @@ function populateExercisePlanFormValues(exercise) {
   /* CREATE LOGIC TO FILL UP */
 
 }
-function toggleEditSetsForm() {
+
+function toggleEditSetsForm(exercise, unchangedSetsWeight) {
   exercisePlanFormContainer.classList.toggle('hide')
   const closeBtn = document.getElementById('close-edit-exercisePlan-form')
   closeBtn.onclick = () => {
     if (confirm('Leave without implement the updates?')) {
+      exercise.setsWeight = unchangedSetsWeight
       exercisePlanFormContainer.classList.toggle('hide')
     }
   }
 }
 async function populateExercisePlanEditForm(exercise) {
-  console.log(exercise)
   exerciseSetsPlan.innerHTML = ''
   exerciseSetsPlan.innerHTML += `
     <label for="editSetWeightUnd">Set the weight <span>und</span> :
@@ -129,7 +139,7 @@ async function populateExercisePlanEditForm(exercise) {
         <input type="number" name="editSet${index + 1}Weight" id="editSet${index + 1}Weight" class="content-card editSetWeightInput" style="font-size: 1.25em"
         placeholder="${Number(setWeigth)}" required>
       </label>
-      <div id="deleteSetWeightInputBtn" class="remove-btn removeSetWeight">
+      <div id="deleteSetWeightInputBtn${index + 1}" class="remove-btn removeSetWeight">
         <svg class="edit" width="25" height="30" viewBox="0 0 207 242" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M5.01928 51.3278H201.066C201.066 51.3278 203.567 51.1478 204.639 50.1407C205.781 49.067 206.021 46.3724 206.021 46.3724V24.6157C206.021 23.4292 205.311 21.2587 203.855 19.7965C202.391 18.3258 200.436 17.6073 199.013 17.6073H134.154V10.2029C134.154 8.5026 132.968 5.1025 131.218 3.31854C129.414 1.47896 125.699 0.299053 123.428 0.299082C114.983 0.29919 109.211 0.282821 103.508 0.266648C97.9304 0.250832 92.4187 0.235202 84.5377 0.235353C82.9606 0.235385 78.66 1.37498 76.8683 3.13737C75.0305 4.94515 73.843 6.88149 73.843 9.00451V17.6073H7.47263C6.11925 17.6073 4.12165 18.7415 2.62848 20.2951C1.23252 21.7475 0.206093 23.2155 0.205457 24.8491C0.203449 30.0067 0.204081 33.783 0.204764 37.8621C0.205201 40.4745 0.205659 43.211 0.205457 46.514C0.205366 48.0038 0.653652 49.7014 1.3468 50.3544C2.38506 51.3326 3.99439 51.3297 4.91223 51.328L5.01928 51.3278Z" fill="white"></path>
           <path fill-rule="evenodd" clip-rule="evenodd" d="M33.6154 241.702H173.793C176.622 241.702 179.027 240.542 181.265 238.23C183.41 236.015 184.477 231.018 184.477 231.018C184.477 231.018 193.914 133.691 199.013 84.0026C199.338 80.832 198.275 78.5999 195.924 76.4481C193.795 74.4997 191.43 73.9569 188.967 73.9569C156.422 73.9569 131.218 73.8884 105.931 73.8197C80.4758 73.7505 54.9365 73.681 21.739 73.681C18.0718 73.681 14.1603 75.063 11.7292 77.3082C9.0262 79.8045 8.50124 83.9913 8.67161 85.5604C14.4598 138.871 22.9311 231.018 22.9311 231.018C22.9311 231.018 24.1434 235.985 26.318 238.192C28.5378 240.444 31.7285 241.702 33.6154 241.702ZM92.9048 119.853C92.9066 140.596 92.9062 165.055 92.9048 194.019C92.9046 197.937 97.4764 202.692 102.515 202.629C107.485 202.567 111.886 197.567 111.886 194.019V119.853C111.886 116.935 107.14 112.669 102.515 112.613C97.8164 112.556 92.9045 116.565 92.9048 119.853ZM52.3855 194.466C50.8711 165.542 49.5914 141.116 48.504 120.402C48.3316 117.118 53.0269 112.857 57.7221 112.668C62.3432 112.482 67.3069 116.494 67.4597 119.408L71.3412 193.472C71.5269 197.016 67.3927 202.239 62.4332 202.561C57.4048 202.888 52.5904 198.378 52.3855 194.466ZM139.935 119.408C138.851 140.123 137.571 164.549 136.053 193.472C135.848 197.385 140.165 202.373 145.2 202.574C150.166 202.772 154.823 198.009 155.009 194.466L158.891 120.402C159.043 117.487 154.526 112.979 149.911 112.681C145.222 112.378 140.107 116.124 139.935 119.408Z" fill="white"></path>
@@ -147,6 +157,7 @@ function populateEditFormInputs(exercise) {
   })
 }
 async function removeSetWeightFeature(exercise, setsWeightPosition) {
+  console.log(exercise)
   console.log(setsWeightPosition)
   let newSetsWeight = exercise.setsWeight.filter((element, index) => index != setsWeightPosition)
   exercise.setsWeight = newSetsWeight
@@ -159,13 +170,22 @@ async function removeSetWeightFeature(exercise, setsWeightPosition) {
     }))
   })
 }
-function addNewSetBtnFeature() {
+function addNewSetBtnFeature(exercise) {
   let newSetIndex = Array.from(exerciseSetsPlan.children).length
   console.log(newSetIndex)
+  exercise.setsWeight.push(0)
   exerciseSetsPlan.innerHTML += `
-    <label for="editSet${newSetIndex}Weight">Weight for set <span>${newSetIndex}</span> :
-      <input type="number" name="editSet${newSetIndex}Weight" id="editSet${newSetIndex}Weight" class="content-card editSetWeightInput" style="font-size: 1.25em" placeholder="0" required>
-    </label>
+    <div style="display: flex; gap: 1em;">
+      <label for="editSet${newSetIndex}Weight">Weight for set <span>${newSetIndex}</span> :
+        <input type="number" name="editSet${newSetIndex}Weight" id="editSet${newSetIndex}Weight" class="content-card editSetWeightInput" style="font-size: 1.25em" placeholder="0" required>
+      </label>
+      <div id="deleteSetWeightInputBtn${newSetIndex}" class="remove-btn removeSetWeight">
+        <svg class="edit" width="25" height="30" viewBox="0 0 207 242" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5.01928 51.3278H201.066C201.066 51.3278 203.567 51.1478 204.639 50.1407C205.781 49.067 206.021 46.3724 206.021 46.3724V24.6157C206.021 23.4292 205.311 21.2587 203.855 19.7965C202.391 18.3258 200.436 17.6073 199.013 17.6073H134.154V10.2029C134.154 8.5026 132.968 5.1025 131.218 3.31854C129.414 1.47896 125.699 0.299053 123.428 0.299082C114.983 0.29919 109.211 0.282821 103.508 0.266648C97.9304 0.250832 92.4187 0.235202 84.5377 0.235353C82.9606 0.235385 78.66 1.37498 76.8683 3.13737C75.0305 4.94515 73.843 6.88149 73.843 9.00451V17.6073H7.47263C6.11925 17.6073 4.12165 18.7415 2.62848 20.2951C1.23252 21.7475 0.206093 23.2155 0.205457 24.8491C0.203449 30.0067 0.204081 33.783 0.204764 37.8621C0.205201 40.4745 0.205659 43.211 0.205457 46.514C0.205366 48.0038 0.653652 49.7014 1.3468 50.3544C2.38506 51.3326 3.99439 51.3297 4.91223 51.328L5.01928 51.3278Z" fill="white"></path>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M33.6154 241.702H173.793C176.622 241.702 179.027 240.542 181.265 238.23C183.41 236.015 184.477 231.018 184.477 231.018C184.477 231.018 193.914 133.691 199.013 84.0026C199.338 80.832 198.275 78.5999 195.924 76.4481C193.795 74.4997 191.43 73.9569 188.967 73.9569C156.422 73.9569 131.218 73.8884 105.931 73.8197C80.4758 73.7505 54.9365 73.681 21.739 73.681C18.0718 73.681 14.1603 75.063 11.7292 77.3082C9.0262 79.8045 8.50124 83.9913 8.67161 85.5604C14.4598 138.871 22.9311 231.018 22.9311 231.018C22.9311 231.018 24.1434 235.985 26.318 238.192C28.5378 240.444 31.7285 241.702 33.6154 241.702ZM92.9048 119.853C92.9066 140.596 92.9062 165.055 92.9048 194.019C92.9046 197.937 97.4764 202.692 102.515 202.629C107.485 202.567 111.886 197.567 111.886 194.019V119.853C111.886 116.935 107.14 112.669 102.515 112.613C97.8164 112.556 92.9045 116.565 92.9048 119.853ZM52.3855 194.466C50.8711 165.542 49.5914 141.116 48.504 120.402C48.3316 117.118 53.0269 112.857 57.7221 112.668C62.3432 112.482 67.3069 116.494 67.4597 119.408L71.3412 193.472C71.5269 197.016 67.3927 202.239 62.4332 202.561C57.4048 202.888 52.5904 198.378 52.3855 194.466ZM139.935 119.408C138.851 140.123 137.571 164.549 136.053 193.472C135.848 197.385 140.165 202.373 145.2 202.574C150.166 202.772 154.823 198.009 155.009 194.466L158.891 120.402C159.043 117.487 154.526 112.979 149.911 112.681C145.222 112.378 140.107 116.124 139.935 119.408Z" fill="white"></path>
+        </svg>
+      </div>
+    </div>
   `
 }
 function getEditSetsWeightInputsValues(exercise, planner, daySplitDoc) {
